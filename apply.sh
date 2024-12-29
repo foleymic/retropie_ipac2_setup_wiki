1684ab7cc362218e2edfb77cec003f4bc028c8de
Mike Foley <foleymic@gmail.com>
2024-12-29 11:46:06 -0500

    testing

--- apply.sh
+++ apply.sh
@@ -5,10 +5,19 @@ SOURCE_DIR="$1"
 DEST_DIR="$2"
 
 # Check if both directories are provided
-if [ -z "$SOURCE_DIR" ] || [ -z "$DEST_DIR" ]; then
-  echo "Usage: $0 <source_directory> <destination_directory>"
-  exit 1
+if [ -z "$SOURCE_DIR" ] ; then
+  echo "Source directory not specified.  Will use default - './retropie_controller_configurations/configs'"
+  SOURCE_DIR="./retropie_controller_configurations/configs"
 fi
+
+if [ -z "$DEST_DIR" ] ; then
+  echo "Destination directory not specified.  Will use default - '/opt/retropie/configs'"
+  DEST_DIR="/opt/retropie/configs"
+fi
+
+echo "Source directory: ${SOURCE_DIR}"
+echo "Destination directory: ${DEST_DIR}"
+
 
 # Check if source directory exists
 if [ ! -d "$SOURCE_DIR" ]; then
@@ -22,26 +31,46 @@ if [ ! -d "$DEST_DIR" ]; then
   mkdir -p "$DEST_DIR"
 fi
 
-# Loop through all files in the source directory
-for FILE in "$SOURCE_DIR"/*; do
-  # Check if it's a regular file
-  if [ -f "$FILE" ]; then
-    # Get the filename
-    FILENAME=$(basename "$FILE")
-    DEST_FILE="$DEST_DIR/$FILENAME"
+# Find all files (not directories) under the source directory
+find "$SOURCE_DIR" -type f | while read -r FILE; do
+  # Get the relative path of the file inside the source directory
+  RELATIVE_PATH="${FILE#$SOURCE_DIR/}"
+  echo "RELATIVE_PATH: ${RELATIVE_PATH}"
 
-    # Check if the destination file exists
-    if [ -e "$DEST_FILE" ]; then
-      # Backup the destination file
-      BACKUP_FILE="$DEST_FILE.bak"
-      echo "Backing up $DEST_FILE to $BACKUP_FILE"
-      mv "$DEST_FILE" "$BACKUP_FILE"
+
+  # Define the destination file path
+  DEST_FILE="$DEST_DIR/$RELATIVE_PATH"
+
+  # Ensure the destination subdirectory exists
+  DEST_SUBDIR=$(dirname "$DEST_FILE")
+  if [ ! -d "$DEST_SUBDIR" ]; then
+    echo "Creating directory $DEST_SUBDIR"
+    mkdir -p "$DEST_SUBDIR"
+  fi
+
+ 
+  # Check if the destination file exists
+  if [ -e "$DEST_FILE" ]; then
+    # Backup the destination file
+
+    BACKUP_SUBDIR="$DEST_SUBDIR/Backups"
+    if [ ! -d "$BACKUP_SUBDIR" ]; then
+        echo "Creating directory $BACKUP_SUBDIR"
+        mkdir -p "$BACKUP_SUBDIR"
     fi
 
-    # Copy the file from source to destination
-    echo "Copying $FILE to $DEST_FILE"
-    cp "$FILE" "$DEST_FILE"
+    DEST_FILE_NAME=$(basename "$DEST_FILE")
+    BACKUP_FILE="$BACKUP_SUBDIR/$DEST_FILE_NAME.bak"
+
+    echo "Backing up $DEST_FILE to $BACKUP_FILE"
+
+    mv "$DEST_FILE" "$BACKUP_FILE"
   fi
+
+  # Copy the file from source to destination
+  echo "Copying $FILE to $DEST_FILE"
+  cp "$FILE" "$DEST_FILE"
+
 done
 
 echo "Files copied successfully!"
---
+++ scripts/runEmulator.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# ***************************************************************************************
+#
+# See: https://sourceforge.net/p/ledspicer/wiki/Emulation%20Station%20Integration/
+#
+# Replace in /etc/emulationsation/es_systems.cfg
+#   i.e.:
+#    /opt/retropie/supplementary/runcommand/runEmulator.sh mame-mame4all %ROM%  "%BASENAME%" arcade
+#    OR
+#    /opt/retropie/supplementary/runcommand/runEmulator.sh n64 %ROM% "%BASENAME%" n64
+#
+# ***************************************************************************************
+
+#    $1 - the system (eg: atari2600, nes, snes, megadrive, fba, etc).
+#    $2 - Rom path
+#    $3 - Rom name
+#    $4 - Platform (arcade, generic, etc)
+
+
+SYSTEM=$1
+ROM_PATH=$2
+ROM_NAME=$3
+PLATFORM=$4
+
+#echo $SYSTEM
+#echo $ROM_PATH
+#echo $ROM_NAME
+#echo $PLATFORM
+
+if [[ "$PLATFORM" == "mame" ||  "$PLATFORM" == "arcade" ]];
+then
+    # SET all arcades to 4-way joystick 
+    # TODO need to be more discriment on this.
+    rotator 1 1 4
+    rotator 2 1 4
+else
+    rotator 1 1 8
+    rotator 2 1 8
+fi
+
+emitter LoadProfileByEmulator "$ROM_NAME" systems/$PLATFORM > /dev/null 2>&1
+
+/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ $SYSTEM "$ROM_PATH"
+
+emitter FinishLastProfile > /dev/null 2>&1
+
+exit 0
